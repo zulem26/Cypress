63c818a010dcac5406901023e56c75d824d15b13
zulem26 <pruvanetastore@gmail.com>
2022-09-27 15:55:59 +0300

    second

--- app.js
+++ app.js
@@ -1,21 +1,65 @@
-//Fonksiyonlar
+// Anonymous Function (isimsiz)
+// function(a, b, c){
+//    return a + b + c
+//}
+// uncaught syntaxError : Function statements reques
 
-function sum(){
-    var num1 = 10
-    var num2 = 20
-    var num3 = 30
-    var total = num1 + num2 + num3
-    return total
+var result =  function (a, b, c){
+    return a + b + c
 }
+console.log(result (1,2,3))
 
-console.log(sum())
-
-
-function sub(){
-    var num4 = 40
-    var num5 = 20
-    var total = num4 - num5
-    return total
+let info = function(fName, lName, age){
+    console.log(fName + ' ' + lName + ' ' + age)
 }
+info('ali', 'can', 26)
 
-console.log(sub())
+// ARROW FUNCTIONS
+function sum(a){
+    return a + 100
+}
+console.log(sum(10))
+
+//function kelimesini ve ismini sil
+// parametre ve body arasina =>
+// () sil. EGER TEK PARAMETRE VAR ISE
+var arrow = a => a+100
+console.log(arrow(10))
+
+function sum2(a,b){
+    return a + b + 100
+}
+console.log(sum2(1,2))
+
+var arrow1 = (a,b) => a + b + 100
+console.log(arrow1(2,3))
+
+// Parametresiz Fonksiyonlarda Arrow
+
+// Traditional Function
+var a = 5, b = 6
+function sum3(){
+    return a + b + 100
+}
+console.log(sum3(5,6))
+
+
+// Anonymous Function
+a = 6
+b = 7
+var newSum = function(){
+    return a + b + 100
+}
+console.log(newSum())
+
+var aro = (a,b) => a + b + 100
+console.log(aro(5,6))
+
+// Prizma Hacim
+var prism1 = function(l, w, h){
+    return l * w * h
+}
+console.log(prism1(1,2,3))
+
+var arrowPrizma =(u,e,h)=>u*e*h
+console.log(arrowPrizma(3,4,2))
---
+++ day01_function1.js
@@ -0,0 +1,21 @@
+//Fonksiyonlar
+
+function sum(){
+    var num1 = 10
+    var num2 = 20
+    var num3 = 30
+    var total = num1 + num2 + num3
+    return total
+}
+
+console.log(sum())
+
+
+function sub(){
+    var num4 = 40
+    var num5 = 20
+    var total = num4 - num5
+    return total
+}
+
+console.log(sub())
